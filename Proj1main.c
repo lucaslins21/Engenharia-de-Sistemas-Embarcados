39b302f6da31fa234cbe21c87d7b3c700323e77b
lucaslins21 <52007148+lucaslins21@users.noreply.github.com>
2024-11-25 15:47:43 -0300

    Update Proj1main.c

--- Proj1main.c
+++ Proj1main.c
@@ -1,24 +1,34 @@
 #include <REG51.H>
 
+sbit controle_baixo = P2^0;
+sbit controle_alto = P2^1;
+
 void main() {
+    unsigned char aux_p1 = 0x01; //Inicializa com 1
+
     while (1) {
-			
-				//verifica o bit 1 de P2 é igual a 1 
-        if (P2 & 0x02) {
-            //copia os 4 bits mais significativos de P0 para P1
-            P1 = (P1 & 0x0F) | (P0 & 0xF0);
+        //verifica se o bit 1 de P2 Ã© igual a 1
+        if (controle_alto) {
+            //atualiza os 4 bits mais significativos de aux_p1 com os de P0
+            aux_p1 = (aux_p1 & 0x0F) | (P0 & 0xF0);
         } else {
-            //zera os 4 bits mais significativos de P1
-            P1 &= 0x0F;
+            //zera os 4 bits mais significativos de aux_p1
+            aux_p1 &= 0x0F;
         }
-			
-        //verifica o bit 0 de P2 é igual a 1 
-        if (P2 & 0x01) {
-            //copia os 4 bits menos significativos de P0 para P1
-            P1 = (P1 & 0xF0) | (P0 & 0x0F);
+
+        //copia o valor calculado para P1
+        P1 = aux_p1;
+
+        //verifica se o bit 0 de P2 Ã© igual a 1
+        if (controle_baixo) {
+            //atualiza os 4 bits menos significativos de aux_p1 com os de P0
+            aux_p1 = (aux_p1 & 0xF0) | (P0 & 0x0F);
         } else {
-            //zera os 4 bits menos significativos de P1
-            P1 &= 0xF0;
+            //zera os 4 bits menos significativos de aux_p1
+            aux_p1 &= 0xF0;
         }
+
+        //copia o valor calculado para P1
+        P1 = aux_p1;
     }
 }
